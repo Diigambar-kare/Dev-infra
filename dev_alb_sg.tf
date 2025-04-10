d6716c564307d5cb4da915e5801ea6946eb2c395
Diigambar-kare <karedigambar@outlook.com>
2025-04-10 12:22:32 +0530

    y1

--- dev_alb_sg.tf
+++ dev_alb_sg.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "dev_alb" {
   name        = "dev-alb"
   description = "Security Group for Load Balancer"
-  vpc_id      = "var.vpc_id"  # Ensure var.vpc_id is correctly passed during apply
+  vpc_id      = var.vpc_id  # Ensure var.vpc_id is correctly passed during apply
 
   egress {
     from_port   = 0
