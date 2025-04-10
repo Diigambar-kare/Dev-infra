4e910d9974268151c17abc1b34638633be506add
Diigambar-kare <karedigambar@outlook.com>
2025-04-10 16:31:12 +0530

    cd

---
+++ main.tf
@@ -0,0 +1,9 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  vpc_cidr            = "10.0.0.0/16"
+  vpc_name            = "fittbot-Dev-VPC"
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnet_cidrs = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+  availability_zones   = ["ap-south-2a", "ap-south-2b", "ap-south-2c"]
+}
