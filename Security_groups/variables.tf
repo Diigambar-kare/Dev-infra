4e95ebe29abd300727a53e7f6bf1bbd7b0e6cc3c
Diigambar-kare <karedigambar@outlook.com>
2025-04-10 13:40:20 +0530

    1

--- Security_groups/variables.tf
+++ Security_groups/variables.tf
@@ -1,4 +1,4 @@
 variable "vpc_id" {
-  type = string
+  description = "vpc-00ac42b122a2c958d"
+  type        = string
 }
-
--- main.tf
+++
@@ -1,4 +0,0 @@
-module "security_groups" {
-  source  = "./Security_groups"
-  vpc_id  = var.vpc_id
-}
