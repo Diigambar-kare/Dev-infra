ba8fc91a5dc4f46eec711872f3f8612d77dbafbe
Diigambar-kare <karedigambar@outlook.com>
2025-04-11 12:27:56 +0530

    k1

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -17,7 +17,7 @@ resource "aws_lb" "dev_lb" {
 resource "aws_lb_target_group" "target_groups" {
   count       = length(var.target_group_names)
   name        = var.target_group_names[count.index]
-  port        = var.target_group_ports[count.inde]
+  port        = var.target_group_ports[count.index]
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
   target_type = "ip"
