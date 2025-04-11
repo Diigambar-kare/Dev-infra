ff2f81a80ee872bfa18d0b14c1402325e86f9228
Diigambar-kare <karedigambar@outlook.com>
2025-04-11 13:16:43 +0530

    k2

--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -1,3 +1,4 @@
+# ALB Security Group
 resource "aws_security_group" "dev_alb" {
   name        = "dev-alb-sg"
   description = "Security Group for Load Balancer"
@@ -10,9 +11,20 @@ resource "aws_security_group" "dev_alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = { Name = "dev-alb-sg"}
+  tags = { Name = "dev-alb-sg" }
 }
 
+# ALB Allow All Traffic
+resource "aws_security_group_rule" "alb_ingress_all" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.dev_alb.id
+}
+
+# ALB Allow HTTP
 resource "aws_security_group_rule" "alb_ingress_http" {
   type              = "ingress"
   from_port         = 3100
@@ -22,6 +34,7 @@ resource "aws_security_group_rule" "alb_ingress_http" {
   security_group_id = aws_security_group.dev_alb.id
 }
 
+# ALB Allow HTTPS
 resource "aws_security_group_rule" "alb_ingress_https" {
   type              = "ingress"
   from_port         = 443
@@ -31,6 +44,7 @@ resource "aws_security_group_rule" "alb_ingress_https" {
   security_group_id = aws_security_group.dev_alb.id
 }
 
+# ALB Allow RDS Connection
 resource "aws_security_group_rule" "alb_ingress_rds" {
   type                     = "ingress"
   from_port                = 3306
@@ -40,6 +54,7 @@ resource "aws_security_group_rule" "alb_ingress_rds" {
   security_group_id        = aws_security_group.dev_alb.id
 }
 
+# ECS Security Group
 resource "aws_security_group" "dev_ecs_sg" {
   name        = "dev-ecs-sg"
   description = "Security Group for ECS"
@@ -55,6 +70,7 @@ resource "aws_security_group" "dev_ecs_sg" {
   tags = { Name = "dev-ecs-sg" }
 }
 
+# ECS Allow ALB Traffic
 resource "aws_security_group_rule" "ecs_ingress_alb" {
   type                     = "ingress"
   from_port                = 8000
@@ -64,6 +80,7 @@ resource "aws_security_group_rule" "ecs_ingress_alb" {
   security_group_id        = aws_security_group.dev_ecs_sg.id
 }
 
+# RDS Security Group
 resource "aws_security_group" "dev_rds_sg" {
   name        = "dev-rds-sg"
   description = "Security Group for RDS"
@@ -79,6 +96,7 @@ resource "aws_security_group" "dev_rds_sg" {
   tags = { Name = "dev-rds-sg" }
 }
 
+# RDS Allow ECS Traffic
 resource "aws_security_group_rule" "rds_ingress_ecs" {
   type                     = "ingress"
   from_port                = 3306
@@ -88,6 +106,7 @@ resource "aws_security_group_rule" "rds_ingress_ecs" {
   security_group_id        = aws_security_group.dev_rds_sg.id
 }
 
+# Launch Wizard Security Group
 resource "aws_security_group" "launch_wizard_sg" {
   name        = "launch-wizard-sg-rds"
   description = "Security Group for Launch Wizard"
@@ -103,6 +122,7 @@ resource "aws_security_group" "launch_wizard_sg" {
   tags = { Name = "launch-wizard-sg-rds" }
 }
 
+# Launch Wizard Allow RDS Access
 resource "aws_security_group_rule" "rds_ingress_launch_wizard" {
   type                     = "ingress"
   from_port                = 3306
@@ -112,13 +132,14 @@ resource "aws_security_group_rule" "rds_ingress_launch_wizard" {
   security_group_id        = aws_security_group.dev_rds_sg.id
 }
 
+# Launch Wizard Allow SSH
 resource "aws_security_group_rule" "launch_wizard_ssh" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
   security_group_id = aws_security_group.launch_wizard_sg.id
-  cidr_blocks       = ["0.0.0.0/0"]  # Open to all, restrict for security
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 # Redis Security Group
@@ -137,6 +158,7 @@ resource "aws_security_group" "dev_redis_sg" {
   tags = { Name = "dev-redis-sg" }
 }
 
+# Redis Allow ECS Access
 resource "aws_security_group_rule" "redis_ingress_ecs" {
   type                     = "ingress"
   from_port                = 6379
@@ -146,6 +168,7 @@ resource "aws_security_group_rule" "redis_ingress_ecs" {
   security_group_id        = aws_security_group.dev_redis_sg.id
 }
 
+# Redis Allow Launch Wizard Access
 resource "aws_security_group_rule" "redis_ingress_launch_wizard" {
   type                     = "ingress"
   from_port                = 6379
