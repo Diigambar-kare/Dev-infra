a3a9dbff4cef3272b040b30394ab348f3307d778
Diigambar-kare <karedigambar@outlook.com>
2025-04-11 12:34:13 +0530

    k1

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ module "alb" {
   source             = "./modules/alb"
   lb_name            = "dev-lb"
   subnets            = ["subnet-0b0e8dda10f4f380d", "subnet-07945cee900c0ca7c", "subnet-01edb5fa9c7dc796d"]
-  security_groups    = ["	dev-alb-sg"]
+  security_groups    = ["dev-alb-sg"]
   vpc_id             = "vpc-0cfe33cec30810558"
   target_group_names = ["dev-green-target-group", "dev-blue-target-group"]  # Ensure list format
 }
